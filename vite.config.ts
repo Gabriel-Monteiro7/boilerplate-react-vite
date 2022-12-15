ab49d8be6e973ca6a80b956a75a3addd2e729f93
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2022-12-15 12:40:54 -0300

    refactor: ajustando os testes para utilizarem o vitest

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,21 @@ import path from 'path'
 // https://vitejs.dev/config/
 //{mode}
 export default defineConfig(() => ({
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
   resolve: {
     alias: [{ find: '~', replacement: path.resolve(__dirname, '/src') }]
   },
-  plugins: [
-    react({
-      jsxRuntime: 'classic'
-    })
-  ],
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./.test/setup.ts'],
+    include: ['**/(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache']
   }
 }))
